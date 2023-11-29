6d80b246c791ea22821c16a154f632fd2c530acb
Ethan Okamura <ethanokamura@Ethans-Mac-mini.local>
2023-11-29 13:17:06 -0800

    fixing tailwind issue

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require('tailwindcss/plugin');
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  purge: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  mode: 'jit',
 	theme: {
 		extend: {
       colors: {

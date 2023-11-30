b1b20aa7c5303fec53bdcddf2ac539bbebb269ea
Ethan Okamura <ethanokamura@Ethans-Macbook-Pro.local>
2023-11-30 10:45:21 -0800

    forgot to delete something

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import { loadEnv } from "vite";
-const { BUILD_TYPE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+// const { BUILD_TYPE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 export default defineConfig({
-  site: BUILD_TYPE == 'https://paperwrld.github.io',
+  site: 'https://paperwrld.github.io',
   // base: 'ethanokamura/',
-  base: BUILD_TYPE == 'ethanokamura/',
+  base: 'ethanokamura/',
   output: 'static',
   integrations: [tailwind()],
 });

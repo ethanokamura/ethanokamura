d4f39ffc910ba108e431643c380062dca2076fb3
Ethan Okamura <ethanokamura@Ethans-Macbook-Pro.local>
2023-11-30 10:26:00 -0800

    prod vs dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { loadEnv } from "vite";
+const { BUILD_TYPE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://paperwrld.github.io',
-  base: 'ethanokamura/',
-  // output: 'static',
+  site: BUILD_TYPE == 'production' ? 'https://paperwrld.github.io' : 'http://localhost:4321',
+  // base: 'ethanokamura/',
+  base: BUILD_TYPE == 'production' ? 'ethanokamura/' : 'dist/',
+  output: 'static',
   integrations: [tailwind()],
-  vite: {
-    ssr: {
-      noExternal: ['tailwind'],
-    }
-  }
 });

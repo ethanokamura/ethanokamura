f620d57ccfba96ee54e288a25a591dfb9b70b39c
Ethan Okamura <ethanokamura@Ethans-Macbook-Pro.local>
2023-11-30 11:24:41 -0800

    temporarily commented out env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import { loadEnv } from "vite";
+// import { loadEnv } from "vite";
 // const { BUILD_TYPE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 export default defineConfig({
   site: 'https://paperwrld.github.io',
-  // base: 'ethanokamura/',
   base: 'ethanokamura/',
   output: 'static',
   integrations: [tailwind()],

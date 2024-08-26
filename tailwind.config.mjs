dc3e39ab30edb68252b2de58c4de09e673dd8450
Ethan <ethanokamura3@gmail.com>
2024-08-25 22:50:51 -0700

    major UI update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,24 +6,13 @@ export default {
 	theme: {
 		extend: {
       colors: {
-        'text': '#ffffff',
+        'text': '#e1e6f0',
         'text2': '#969eb0',
         'text3': '#666e7d',
         'background': '#10141a',
         'accent': '#1d2530',
         'primary': '#61AFEF',
         'secondary': '#151b24',
-        theme: {
-          300: '#ac83fc',
-          200: '#71de9e',
-          100: '#de7571'
-        },
-        green: {
-          100:  '#98C379',
-        },
-        red: {
-          100: '#E06C75',
-        },
       },
       dropShadow: {
         black: [
@@ -32,15 +21,8 @@ export default {
         glow: [
           "0px 0px 7px #2f80b5"
         ],
-        redglow: [
-          "0px 0px 7px #d94b41"
-        ],
-        greenglow: [
-          "0px 0px 7px #5df098"
-        ]
       },
       fontFamily: {
-        brains: 'brains',
         rubik: 'rubik',
       },
       animation: {
@@ -49,17 +31,6 @@ export default {
         blink: "blink 0.75s infinite"
       },
       keyframes: {
-        title: {
-          '0%': {
-            textShadow: '0.02em 0.02em #d94b41, 0.04em 0.04em #f07f3a, 0.06em 0.06em #5df098, 0.08em 0.08em #3a83f0, 0.1em 0.1em #7d3fe8'
-          },
-          '60%': {
-            textShadow: '0.04em 0.04em #d94b41, 0.08em 0.08em #f07f3a, 0.12em 0.12em #5df098, 0.16em 0.16em #3a83f0, 0.2em 0.2em #7d3fe8'
-          },
-          '100%': {
-            textShadow: '0.02em 0.02em #d94b41, 0.04em 0.04em #f07f3a, 0.06em 0.06em #5df098, 0.08em 0.08em #3a83f0, 0.1em 0.1em #7d3fe8'
-          },
-        },
         typing: {
           from: {
             width: "0",
